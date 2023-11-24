980b8aaedc9da443aec0401596ae09f57462ea5e
Jemal Khachidze <jemal.khachidze@fullhaus.de>
2023-11-24 15:55:45 +0100

    4-Back-End-Development, 4.1-Introduction to Back-End Development, 4.2-Build a Back-End with Express.js
    Signed-off-by: Jeko92 <jkhachidze92@gmail.com>

---
+++ 4-Back-End-Development/4.2_Build-a-Back-end-with-Express.js/4.2.4_Learn-Express-Middleware/4.2.4.4_Lesson-Express-Middleware-Code-Challenges/03_09_challenge.js
@@ -0,0 +1,39 @@
+/*
+Complete the timeMiddleware function to DRY the current application
+by attaching the currentTime to the request body as date.
+Use this in all routes and eliminate the duplicate code
+ */
+const express = require("express");
+const app = express();
+
+const database = {
+  snacks: ["chips", "apple", "cookies"],
+  sides: ["beans and rice", "cole slaw", "broccoli"],
+  appetizers: ["oysters", "dumplings", "smoked almonds"],
+};
+
+// Add your code here:
+const timeMiddleware = (req, res, next) => {
+  req.date = Date.now();
+  next();
+};
+
+app.use(timeMiddleware);
+
+app.get("/snacks", (req, res, next) => {
+  //   const currentTime = Date.now();
+  //   res.send(`Snacks as of ${currentTime}: ${database.snacks}`);
+  res.send(`Snacks as of ${req.date}: ${database.snacks}`);
+});
+
+app.get("/sides", (req, res, next) => {
+  //   const currentTime = Date.now();
+  //   res.send(`Sides as of ${currentTime}: ${database.sides}`);
+  res.send(`Sides as of ${req.date}: ${database.sides}`);
+});
+
+app.get("/appetizers", (req, res, next) => {
+  //   const currentTime = Date.now();
+  //   res.send(`Appetizers as of ${currentTime}: ${database.appetizers}`);
+  res.send(`Appetizers as of ${req.date}: ${database.appetizers}`);
+});
---
+++ 4-Back-End-Development/4.2_Build-a-Back-end-with-Express.js/4.2.4_Learn-Express-Middleware/4.2.4.4_Lesson-Express-Middleware-Code-Challenges/05_09_challenge.js
@@ -0,0 +1,19 @@
+/*
+Import the morgan logging middleware to log all requests.
+Use the predefined format ‘short’ instead of ‘tiny’.
+ */
+const express = require("express");
+const app = express();
+// Add your code below:
+const morgan = require("morgan");
+
+// Add your code above
+app.use(morgan("short"));
+
+app.get("/say-hi", (req, res, next) => {
+  res.send("Hi!");
+});
+
+app.get("/say-bye", (req, res, next) => {
+  res.send("Bye!");
+});
